ce9e640cd7990e3b260a48d7ce347ea9c2b91b3f
Ilya Ivashko <ivashko-ilya@mail.ru>
2020-03-22 17:21:53 +0300

    feat: add function min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || array.length < 1) {
+    return 0;
+  }
+  var result = array[0];
+  for (var i = 1; i < array.length; i++){
+    if (array[i] < result){
+      result = array[i];
+    }
+  }
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length < 1) {
+        return 0;
+    }
+    var result = array[0];
+    for (var i = 1; i < array.length; i++){
+        if (array[i] > result){
+            result = array[i];
+        }
+    }
+    return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!Array.isArray(array) || array.length < 1) {
+        return 0;
+    }
+    var result = 0;
+    for (var i = 0; i < array.length; i++){
+        result += array[i];
+    }
+    return result / array.length;
 }
